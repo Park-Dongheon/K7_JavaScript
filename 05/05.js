68163f81f93b8ff1be1229ad960a640787708a15
Park-Dongheon <cdsded@naver.com>
2024-04-26 18:00:56 +0900

    "js_day10"

--- 05/05.js
+++ 05/05.js
@@ -14,10 +14,18 @@
         -초기화: 랜덤수 새로 생성, input 박스 보이기...
 */
 
+// 화면 이미지 그리기
+const showImg = (imgName) => {
+    const updownImg = document.querySelector('#what');
+
+    updownImg.setAttribute('src', `./img/${imgName}.png`);
+    updownImg.setAttribute('alt', `${imgName}`);
+};
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const updownImg = document.querySelector('#what');
+
     const bt = document.querySelector('#bt');
     const input = document.querySelector('#txt1');
     
@@ -26,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // 랜덤수
     let n;
     let flag = false;
+    let imgName;
 
     bt.addEventListener('click', (e)=> {
         e.preventDefault();
@@ -34,39 +43,57 @@ document.addEventListener("DOMContentLoaded", () => {
         if(!flag){
             flag = true;
             n = Math.floor(Math.random()*100) + 1; // 1~100
-            console.log(n);
+            console.log("랜덤 값 생성 : " + n);
+
+            showImg('what');
+            input.style.display = 'inline';
+            bt.textContent = '확인';
         }
 
         // 입력값 체크
         if(input.value == "") {
             alert('값을 입력하세요');
             input.focus();
-            input.value = null;
             return;
         }
 
-        if(input.value < 1 || input.value > 100) {
-            alert('1~100의 숫자를 입력하시오');
+
+        // 입력값 체크: 1 ~ 100
+        const user = parseInt(input.value);
+        console.log(user);
+        if(user < 1 || user > 100) {
+            alert('[입력오류] 1~100의 숫자를 입력하시오');
             input.focus();
-            input.value = null;
             return;
         }
 
-        if(input.value < n) {
-            updownImg.setAttribute('src', `./img/up.png`);
-            input.focus();
+        // 메시지 출력
+        // -input 박스 값이 랜덤수 보다 작으면 Up이미지
+        // -input 박스 값이 랜덤수 보다 크면 Down이미지
+        // -input 박스 값이 랜덤수 보다 같으면 Good이미지
+
+        if(user < n) {
+            imgName = 'up';
+        }
+        else if(user > n) {
+            imgName = 'down';
+        }
+        else {
+            imgName = 'good';
+
+            input.style.display = 'none';
+            bt.textContent = '숫자를 다시 생성해 주세요.';
+            flag = false; 
+        }
+
+        if(imgName === 'up' || imgName === 'down') {
             input.value = null;
-        }
-        else if(input.value > n) {
-            updownImg.setAttribute('src', `./img/down.png`);
-            input.focus();
-            input.value = null;
-        }
-        else if(input.value == n) {
-            updownImg.setAttribute('src', `./img/good.png`);
             input.focus();
         }
-        
+
+        input.value = null;
+        showImg(imgName);
+
     });
 
 });
